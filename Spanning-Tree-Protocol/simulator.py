e50761f7ac717498d2ef93a35b182fe19b6ee893
Hassan Ramadan <HassanRamadanEbrahim@gmail.com>
2021-10-27 14:50:03 +0200

    Add spanning tree protocol programming challenge

---
+++ Spanning-Tree-Protocol/simulator.py
@@ -0,0 +1,22 @@
+from topology_reader import bridge_initializer
+
+
+def simulator(bridges, end_time):
+    # Initializing bridges
+    bridges_dict = {}
+    bridges_dict = bridge_initializer(bridges)  # bridge ID : bridge
+
+    print("before STP")
+    for b in bridges_dict:
+        bridges_dict[b].print_bridge()
+
+    # Calling bridges in round robin fashion
+    for t in range(end_time):
+        for b in bridges_dict:
+            bridges_dict = bridges_dict[b].send_BPDUs(bridges_dict)
+        for b in bridges_dict:
+            bridges_dict = bridges_dict[b].receive_BPDUs(bridges_dict)
+
+    print("after STP")
+    for b in bridges_dict:
+        bridges_dict[b].print_bridge()
